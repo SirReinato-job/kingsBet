01e16014d4cf545014d3795b9cad28c65037f71d
SirReinato <renatofranca2881@gmail.com>
2025-06-04 20:08:57 -0300

    Ajustando painel, acrescentando valor gasto e valor ganho

--- src/components/PainelBet/index.jsx
+++ src/components/PainelBet/index.jsx
@@ -1,15 +1,20 @@
-import styles from './PainelBet.module.scss';
-import { useBet } from '../../context/BetContext';
-import ModalGanho from '../ModalGanho';
-import { useState } from 'react';
-
+import styles from "./PainelBet.module.scss";
+import { useBet } from "../../context/BetContext";
+import ModalGanho from "../ModalGanho";
+import { useState } from "react";
 
 export default function PainelBet() {
     const [modalIsOpen, setModalIsOpen] = useState(false);
 
-
     // Obtém os valores do contexto BetContext
-    const { saldoInicial, saldoAtual, setSaldoAtual, valorGiro, resetarDados, registrarGiro } = useBet();
+    const {
+        saldoInicial,
+        saldoAtual,
+        valorGiro,
+        resetarDados,
+        registrarGiro,
+        historico,
+    } = useBet();
 
     function openModal() {
         setModalIsOpen(true);
@@ -24,49 +29,79 @@ export default function PainelBet() {
         if (saldoAtual >= valorGiro && valorGiro > 0) {
             registrarGiro(saldoAtual);
         } else {
-            alert('Saldo insuficiente, por favor, acrescente um valor primeiro.');
+            alert(
+                "Saldo insuficiente, por favor, acrescente um valor primeiro."
+            );
         }
     }
-
 
     return (
         <div className={styles.painelBetConteiner}>
             <h2 className={styles.tituloPainel}>Painel de Controle</h2>
             <div className={styles.painelBet}>
-
                 <div className={styles.painelBetItem}>
-                    <h3 className={styles.tituloPainelBetItem}>Valor inicial</h3>
+                    <h3 className={styles.tituloPainelBetItem}>
+                        Valor inicial
+                    </h3>
                     <span className={styles.valorPainelBetItem}>
-                        {'R$ ' + saldoInicial.toFixed(2)}
+                        {"R$ " + saldoInicial.toFixed(2)}
                     </span>
                 </div>
                 <div className={styles.painelBetItem}>
                     <h3 className={styles.tituloPainelBetItem}>Lucro Total</h3>
                     <span className={styles.valorPainelBetItem}>
-                        {'R$ ' + (saldoAtual).toFixed(2)}
+                        {"R$ " + saldoAtual.toFixed(2)}
                     </span>
-
                 </div>
                 <div className={styles.painelBetItem}>
                     <h3 className={styles.tituloPainelBetItem}>Porcentagem</h3>
                     <span className={styles.valorPainelBetItem}>
-                        {((saldoAtual - saldoInicial) / saldoInicial * 100).toFixed(2) + '%'}
+                        {(
+                            ((saldoAtual - saldoInicial) / saldoInicial) *
+                            100
+                        ).toFixed(2) + "%"}
                     </span>
+                </div>
+            </div>
+            <div className={styles.painelBet}>
+                <div className={styles.painelBetItem}>
+                    <h3 className={styles.tituloPainelBetItem}>Valor Gasto</h3>
+                    <span className={styles.valorPainelBetItem}>
+                        {"R$ " + (historico.length * valorGiro).toFixed(2)}
+                    </span>
+                </div>
 
+                <div className={styles.painelBetItem}>
+                    <h3 className={styles.tituloPainelBetItem}>Valor Ganho</h3>
+                    <span className={styles.valorPainelBetItem}>
+                        {historico
+                            .filter((item) => item.tipo === "ganho")
+                            .reduce((acc, item) => acc + item.valor, 0)
+                            .toFixed(2)}
+                    </span>
                 </div>
             </div>
 
-
             <div className={styles.painelBtnConteiner}>
+                <button
+                    onClick={() => registrarSeuGiro()}
+                    className={styles.btnApostar}
+                >
+                    Apostar
+                </button>
 
-                <button onClick={() => registrarSeuGiro()} className={styles.btnApostar}>Apostar</button>
+                <button
+                    onClick={() => resetarDados()}
+                    className={styles.btnReiniciar}
+                >
+                    Reiniciar
+                </button>
 
-                <button onClick={() => resetarDados()} className={styles.btnReiniciar}>Reiniciar</button>
-
-                <button onClick={openModal} className={styles.btnGanho}>Green</button>
+                <button onClick={openModal} className={styles.btnGanho}>
+                    Green
+                </button>
             </div>
             <ModalGanho isOpen={modalIsOpen} onClose={closeModal} />
-
         </div>
     );
 }
